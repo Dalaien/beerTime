3d0a81f1162cbdaa55e260b30fedc7a159b0c72c
Dalaien <alessio.danesi@laposte.net>
2021-12-02 17:21:49 +0100

    First entities

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,18 +8,22 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/event', name: 'event_')]
 class EventController extends AbstractController
 {
+
     #[Route('', name: 'list')]
     public function list(): Response
-    {return new Response("liste des événements");
+    {
+        return new Response("Page liste d'événements");
     }
 
-    #[Route('/{id}', name: 'show', requirements: ['id' =>'\d+'])]
+    #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'])]
     public function show($id): Response
-    {return new Response("Voir un événement");
+    {
+        return new Response("Page vu d'un événement : " . $id);
     }
 
     #[Route('/new', name: 'new')]
     public function new(): Response
-    {return new Response("Page de création d'un événement");
+    {
+        return new Response("Page de création d'un événement");
     }
 }
---
+++ src/Entity/Place.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlaceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PlaceRepository::class)
+ */
+class Place
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $street;
+
+    /**
+     * @ORM\Column(type="string", length=12)
+     */
+    private $zipcode;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=2)
+     */
+    private $country;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): self
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getZipcode(): ?string
+    {
+        return $this->zipcode;
+    }
+
+    public function setZipcode(string $zipcode): self
+    {
+        $this->zipcode = $zipcode;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $counrty): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+}
