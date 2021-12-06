b8526ad503779cf5ecae574b65ed82d9eaa5d036
Dalaien <alessio.danesi@laposte.net>
2021-12-06 09:32:15 +0100

    update symfony

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -8,17 +9,31 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/event', name: 'event_')]
 class EventController extends AbstractController
 {
+    private $eventRepository;
+
+    public function __construct(EventRepository $eventRepository)
+    {
+        $this->eventRepository = $eventRepository;
+
+    }
 
     #[Route('', name: 'list')]
     public function list(): Response
     {
-        return new Response("Page liste d'événements");
+            $events= $this ->eventRepository ->findall();
+        return $this->render('/event/list.html.twig',[
+            'events' => $events
+        ]);
     }
 
     #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'])]
     public function show($id): Response
     {
-        return new Response("Page vu d'un événement : " . $id);
+        $event =$this->eventRepository ->find($id);
+        return $this->render('/event/show.html.twig',[
+            'event'=> $event
+        ]);
+       // new Response("Page vu d'un événement : " . $id);
     }
 
     #[Route('/new', name: 'new')]
